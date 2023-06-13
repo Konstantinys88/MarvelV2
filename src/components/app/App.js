36d5f03855d7582010d87eb6a39395fe8102a2b2
Konstantin0 <maschkovc@yandex.ru>
2023-06-13 11:58:01 +0300

    charList

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,16 +1,20 @@
 import Header from '../header/Header';
 import RandomCharacter from '../randomCharacter/RandomCharacter';
+import CharList from '../charList/CharList';
 
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <RandomCharacter/>
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<RandomCharacter />
+			<div className='App__charList'>
+				<CharList />
+			</div>
+
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/charList/CharList.js
@@ -0,0 +1,49 @@
+import abus from "../../img/abus.svg";
+import "./charList.scss";
+
+const CharList = () => {
+    return (
+        <div className="charList">
+            <ul>
+                <li className="charList__item">
+                    <img src={abus} alt="charImg" />
+                    <h4>ABYSS</h4>
+                </li>
+                <li className="charList__item">
+                    <img src={abus} alt="charImg" />
+                    <h4>ABYSS</h4>
+                </li>
+                <li className="charList__item">
+                    <img src={abus} alt="charImg" />
+                    <h4>ABYSS</h4>
+                </li>
+                <li className="charList__item">
+                    <img src={abus} alt="charImg" />
+                    <h4>ABYSS</h4>
+                </li>
+                <li className="charList__item">
+                    <img src={abus} alt="charImg" />
+                    <h4>ABYSS</h4>
+                </li>
+                <li className="charList__item">
+                    <img src={abus} alt="charImg" />
+                    <h4>ABYSS</h4>
+                </li>
+                <li className="charList__item">
+                    <img src={abus} alt="charImg" />
+                    <h4>ABYSS</h4>
+                </li>
+                <li className="charList__item">
+                    <img src={abus} alt="charImg" />
+                    <h4>ABYSS</h4>
+                </li>
+                <li className="charList__item">
+                    <img src={abus} alt="charImg" />
+                    <h4>ABYSS</h4>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default CharList;
